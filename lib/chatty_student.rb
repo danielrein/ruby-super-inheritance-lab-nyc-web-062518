63f4fb811db4c66ecd7d5fc6c4f1215faf09be5f
Daniel Ran <danielr1735@gmail.com>
2018-07-03 17:28:43 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,6 @@
 class ChattyStudent < Student
   def hello
-    "Hello"
+    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don
+'t want any spoilers? Okay well let me just tell you who died...\n"
   end
 end
